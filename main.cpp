c0d5d2ce36c877ce7519492a065cab094e722b0a
NamKwanWoo <namnamnam@namgwanuui-MacBook-Pro.local>
2016-06-02 21:24:53 +0900

    File open Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("Hello");
+    FILE *user = fopen("/Users/namnamnam/Desktop/KOREAUNIV_DS_03/user.utf8", "r");
+    FILE *fren = fopen("/Users/namnamnam/Desktop/KOREAUNIV_DS_03/friend.utf8", "r");
+    FILE *word = fopen("/Users/namnamnam/Desktop/KOREAUNIV_DS_03/word.utf8", "r");
+    
+    if(user == NULL || fren == NULL || word == NULL)
+    {
+        puts("File does not exits!");       // error
+        exit(-1);                           // exit
+    }
+    
+    char *str = (char*)malloc(sizeof(char) * 1000);
+    int i;
+    
+    while(fgets(str, 1000, user) && i<1000)
+        printf("%s", str);
+
+    while(fgets(str, 1000, fren) && i<1000)
+        printf("%s", str);
+
+    while(fgets(str, 1000, word) && i<1000)
+        printf("%s", str);
+    
+    
+    return 0;
 }
